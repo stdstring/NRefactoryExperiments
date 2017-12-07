f0879deca62e47d8be09f97bf326c3681c95a887
Ushakov Andrey <std_string@mail.ru>
2017-12-07 12:31:17 +0500

    moved existing code from external place

---
+++ NRefactoryExperiments/StructureResearchTests/AttributesTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.CSharp.Resolver;
+using ICSharpCode.NRefactory.CSharp.TypeSystem;
+using ICSharpCode.NRefactory.Semantics;
+using ICSharpCode.NRefactory.TypeSystem;
+using Mono.Cecil;
+using NUnit.Framework;
+using Attribute = ICSharpCode.NRefactory.CSharp.Attribute;
+
+namespace StructureResearchTests
+{
+    [TestFixture]
+    public class AttributesTests
+    {
+        [Test]
+        public void CheckAttributes()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    [System.AttributeUsage(System.AttributeTargets.Method | System.AttributeTargets.Property, AllowMultiple = true)]\r\n" +
+                                  "    public class AttrA : System.Attribute\r\n" +
+                                  "    {\r\n" +
+                                  "    }\r\n" +
+                                  "    [System.AttributeUsage(System.AttributeTargets.Method)]\r\n" +
+                                  "    public class AttrB : System.Attribute\r\n" +
+                                  "    {\r\n" +
+                                  "    }\r\n" +
+                                  "    public class SomeClass\r\n" +
+                                  "    {\r\n" +
+                                  "        [AttrA]\r\n" +
+                                  "        [AttrB]\r\n" +
+                                  "        public void M()\r\n" +
+                                  "        { }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            CSharpParser parser = new CSharpParser();
+            SyntaxTree syntaxTree = parser.Parse(source);
+            MethodDeclaration method = syntaxTree.Descendants.OfType<MethodDeclaration>().First(m => m.Name == "M");
+            AstNodeCollection<AttributeSection> sections = method.Attributes;
+            Console.WriteLine("sections.Count = {0}", sections.Count);
+            Console.WriteLine();
+            foreach (AttributeSection section in sections)
+            {
+                Console.WriteLine("section.AttributeTarget = {0}", section.AttributeTarget);
+                AstNodeCollection<Attribute> attributes = section.Attributes;
+                Console.WriteLine("attributes.Count = {0}", attributes.Count);
+                foreach (Attribute attribute in attributes)
+                {
+                    Console.WriteLine("((SimpleType) attribute.Type).Identifier = {0}", ((SimpleType) attribute.Type).Identifier);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        [Test]
+        public void ResolveTypeWithUnknownAttributes()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public enum EE {v1 = 13, v2 = 666}\r\n" +
+                                  "    [System.AttributeUsage(System.AttributeTargets.Method | System.AttributeTargets.Property, AllowMultiple = true)]\r\n" +
+                                  "    public class AttrA : System.Attribute\r\n" +
+                                  "    {\r\n" +
+                                  "    }\r\n" +
+                                  "    [System.AttributeUsage(System.AttributeTargets.Method)]\r\n" +
+                                  "    public class AttrB : System.Attribute\r\n" +
+                                  "    {\r\n" +
+                                  "        public AttrB(int i, string s, EE e) {}\r\n" +
+                                  "    }\r\n" +
+                                  "    public class SomeClass\r\n" +
+                                  "    {\r\n" +
+                                  "        [AttrA]\r\n" +
+                                  "        [AttrB(666, \"iddqd\", EE.v1)]\r\n" +
+                                  "        [AttrC]\r\n" +
+                                  "        public void M()\r\n" +
+                                  "        { }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            CSharpParser parser = new CSharpParser();
+            SyntaxTree syntaxTree = parser.Parse(source);
+            syntaxTree.FileName = "example.cs";
+            CSharpUnresolvedFile unresolvedTypeSystem = syntaxTree.ToTypeSystem();
+            IProjectContent content = new CSharpProjectContent();
+            content = content.AddOrUpdateFiles(unresolvedTypeSystem);
+            CecilLoader loader = new CecilLoader();
+            AssemblyDefinition mscorlibAssemblyDefinition = AssemblyDefinition.ReadAssembly(typeof(Object).Assembly.Location);
+            IUnresolvedAssembly mscorlibAssembly = loader.LoadAssembly(mscorlibAssemblyDefinition);
+            content = content.AddAssemblyReferences(mscorlibAssembly);
+            ICompilation compilation = content.CreateCompilation();
+            CSharpAstResolver resolver = new CSharpAstResolver(compilation, syntaxTree);
+            MethodDeclaration method = syntaxTree.Descendants.OfType<MethodDeclaration>().First(m => m.Name == "M");
+            ResolveResult result = resolver.Resolve(method);
+            MemberResolveResult memberResult = (MemberResolveResult) result;
+            IMember member = memberResult.Member;
+            foreach (IAttribute attribute in member.Attributes)
+            {
+                Console.WriteLine("attribute.AttributeType = {0}, attribute.AttributeType.Kind = {1}", attribute.AttributeType.FullName, attribute.AttributeType.Kind);
+                Console.WriteLine("attribute.PositionalArguments.Count = {0}", attribute.PositionalArguments.Count);
+                ProcessPositionalArgs(attribute.PositionalArguments);
+                Console.WriteLine("attribute.NamedArguments.Count = {0}", attribute.NamedArguments.Count);
+                Console.WriteLine();
+            }
+        }
+
+        [Test]
+        public void MethodParamsReturnAttributes()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    [System.AttributeUsage(System.AttributeTargets.Parameter)]\r\n" +
+                                  "    public class AttrA : System.Attribute\r\n" +
+                                  "    {\r\n" +
+                                  "    }\r\n" +
+                                  "    [System.AttributeUsage(System.AttributeTargets.ReturnValue)]\r\n" +
+                                  "    public class AttrB : System.Attribute\r\n" +
+                                  "    {\r\n" +
+                                  "    }\r\n" +
+                                  "    public class SomeClass\r\n" +
+                                  "    {\r\n" +
+                                  "        [return: AttrB]\r\n" +
+                                  "        public string SomeMethod(int p1, [AttrA]string p2) { return \"iddqd\"; }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            CSharpParser parser = new CSharpParser();
+            SyntaxTree syntaxTree = parser.Parse(source);
+            syntaxTree.FileName = "example.cs";
+            CSharpUnresolvedFile unresolvedTypeSystem = syntaxTree.ToTypeSystem();
+            IProjectContent content = new CSharpProjectContent();
+            content = content.AddOrUpdateFiles(unresolvedTypeSystem);
+            CecilLoader loader = new CecilLoader();
+            AssemblyDefinition mscorlibAssemblyDefinition = AssemblyDefinition.ReadAssembly(typeof(Object).Assembly.Location);
+            IUnresolvedAssembly mscorlibAssembly = loader.LoadAssembly(mscorlibAssemblyDefinition);
+            content = content.AddAssemblyReferences(mscorlibAssembly);
+            ICompilation compilation = content.CreateCompilation();
+            CSharpAstResolver resolver = new CSharpAstResolver(compilation, syntaxTree);
+            MethodDeclaration method = syntaxTree.Descendants.OfType<MethodDeclaration>().First(m => m.Name == "SomeMethod");
+            ResolveResult result = resolver.Resolve(method);
+            MemberResolveResult memberResult = (MemberResolveResult)result;
+            IMember member = memberResult.Member;
+        }
+
+        private void ProcessPositionalArgs(IList<ResolveResult> args)
+        {
+            if (args.Count == 0)
+                return;
+            Console.WriteLine("attribute.PositionalArguments :");
+            for (Int32 index = 0; index < args.Count; ++index)
+            {
+                ResolveResult argResult = args[index];
+                Console.Write("index = {0}, type = {1}", index, argResult.GetType());
+                if (argResult is ConstantResolveResult)
+                {
+                    ConstantResolveResult constResult = (ConstantResolveResult) argResult;
+                    Console.Write(", value = {0}", constResult.ConstantValue);
+                }
+                if (argResult is MemberResolveResult)
+                {
+                    MemberResolveResult memberResult = (MemberResolveResult) argResult;
+                    IMember member = memberResult.Member;
+                    Console.Write(", value = {0}, memberKind = {1}, typeKind = {2}", member.Name, member.SymbolKind, memberResult.Type.Kind);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ NRefactoryExperiments/StructureResearchTests/BaseReferenceTests.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Linq;
+using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.CSharp.Resolver;
+using ICSharpCode.NRefactory.CSharp.TypeSystem;
+using ICSharpCode.NRefactory.Semantics;
+using ICSharpCode.NRefactory.TypeSystem;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace StructureResearchTests
+{
+    [TestFixture]
+    public class BaseReferenceTests
+    {
+        [Test]
+        public void BaseMethodInvocation()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void M() { }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "    }\r\n" +
+                                  "    public class C : B\r\n" +
+                                  "    {\r\n" +
+                                  "    public void Do() { base.M(); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void OverloadBaseMethodInvocation()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void M() { }\r\n" +
+                                  "        public void M(string s, int i) { }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void M(int i) { }\r\n" +
+                                  "        public void M(string s) { }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class C : B\r\n" +
+                                  "    {\r\n" +
+                                  "        public void Do() { base.M(\"iddqd\", 666); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBaseFieldInMethodCall()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class S\r\n" +
+                                  "    {\r\n" +
+                                  "        public static void Do(string s, int i) { }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int value;\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void M() { S.Do(\"iddqd\", base.value); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBasePropertyInMethodCall()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class S\r\n" +
+                                  "    {\r\n" +
+                                  "        public static void Do(string s, int i) { }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int Value { get; set; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void M() { S.Do(\"iddqd\", base.Value); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBaseMethodInMethodCall()
+        {
+            const String source = "namespace nsnamespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class S\r\n" +
+                                  "    {\r\n" +
+                                  "        public static void Do(string s, int i) {}\r\n" +
+                                  "    }\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int GetValue() { return 666; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void M() { S.Do(\"iddqd\", base.GetValue()); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBaseOverloadMethodInMethodCall()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class S\r\n" +
+                                  "    {\r\n" +
+                                  "        public static void Do(string s, int i) { }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int GetValue() { return 666; }\r\n" +
+                                  "        public int GetValue(string s, int i) { return 667; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int GetValue(int i) { return 13; }\r\n" +
+                                  "        public int GetValue(string s) { return 17; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class C : B\r\n" +
+                                  "    {\r\n" +
+                                  "        public void M() { S.Do(\"iddqd\", base.GetValue(\"idkfa\", 999)); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBaseFieldInExpression()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int value;\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void Do() { System.Console.WriteLine(13 + base.value * 2); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBasePropertyInExpression()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int Value { get; set; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void Do() { System.Console.WriteLine(13 + base.Value * 2); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBaseMethodInExpression()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int GetValue() { return 666; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public void Do() { System.Console.WriteLine(13 + base.GetValue() * 2); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        [Test]
+        public void UseBaseOverloadMethodInExpression()
+        {
+            const String source = "namespace ns\r\n" +
+                                  "{\r\n" +
+                                  "    public class A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int GetValue() { return 666; }\r\n" +
+                                  "        public int GetValue(string s, int i) { return 667; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class B : A\r\n" +
+                                  "    {\r\n" +
+                                  "        public int GetValue(int i) { return 13; }\r\n" +
+                                  "        public int GetValue(string s) { return 17; }\r\n" +
+                                  "    }\r\n" +
+                                  "    public class C : B\r\n" +
+                                  "    {\r\n" +
+                                  "        public void Do() { System.Console.WriteLine(13 + 2 * base.GetValue(\"idkfa\", 999)); }\r\n" +
+                                  "    }\r\n" +
+                                  "}";
+            TestCommonBody(source);
+        }
+
+        private void TestCommonBody(String source)
+        {
+            CSharpParser parser = new CSharpParser();
+            SyntaxTree syntaxTree = parser.Parse(source);
+            syntaxTree.FileName = "example.cs";
+            BaseReferenceExpression baseRef = syntaxTree.Descendants.OfType<BaseReferenceExpression>().First();
+            ExpressionStatement expr = MoveToParent<ExpressionStatement>(baseRef);
+            CSharpUnresolvedFile unresolvedTypeSystem = syntaxTree.ToTypeSystem();
+            IProjectContent content = new CSharpProjectContent();
+            content = content.AddOrUpdateFiles(unresolvedTypeSystem);
+            CecilLoader loader = new CecilLoader();
+            AssemblyDefinition mscorlibAssemblyDefinition = AssemblyDefinition.ReadAssembly(typeof(Object).Assembly.Location);
+            IUnresolvedAssembly mscorlibAssembly = loader.LoadAssembly(mscorlibAssemblyDefinition);
+            content = content.AddAssemblyReferences(mscorlibAssembly);
+            ICompilation compilation = content.CreateCompilation();
+            CSharpAstResolver resolver = new CSharpAstResolver(compilation, syntaxTree);
+            ShowSubtree(expr, 0, resolver);
+        }
+
+        private TNode MoveToParent<TNode>(AstNode node) where TNode : AstNode
+        {
+            AstNode parent = node;
+            while (parent != null)
+            {
+                if (parent.GetType() == typeof (TNode))
+                    return (TNode) parent;
+                parent = parent.Parent;
+            }
+            return null;
+        }
+
+        private void ShowSubtree(AstNode node, Int32 indent, CSharpAstResolver resolver)
+        {
+            ResolveResult result = resolver.Resolve(node);
+            Console.WriteLine("{0} node type = {1}, value {2}, resolve result = {3}", new String(' ', indent), node.GetType().Name, Prepare(node.ToString()), GetResolveResultRepresentation(result));
+            foreach (AstNode child in node.Children)
+                ShowSubtree(child, indent + IndentDelta, resolver);
+        }
+
+        private String Prepare(String source)
+        {
+            return source.Replace("\r", "\\r").Replace("\n", "\\n");
+        }
+
+        private String GetResolveResultRepresentation(ResolveResult result)
+        {
+            if (result is MethodGroupResolveResult)
+            {
+                String methodsRepresentation = String.Join(", ", ((MethodGroupResolveResult) result).Methods.Select(m => m.ToString()));
+                return String.Concat(result, " : ", methodsRepresentation);
+            }
+            return result.ToString();
+        }
+
+        private const Int32 IndentDelta = 2;
+    }
+}
+
+/*namespace ns
+{
+    public class A
+    {
+        public int GetValue() { return 666; }
+        public int GetValue(string s, int i) { return 667; }
+    }
+
+    public class B : A
+    {
+        public int GetValue(int i) { return 13; }
+        public int GetValue(string s) { return 17; }
+    }
+
+    public class C : B
+    {
+        public void Do() { System.Console.WriteLine(13 + 2 * base.GetValue("idkfa", 999)); }
+    }
+}*/
